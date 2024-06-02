bb5da2a648ff2c173586f70842e03961ed916e89
Arsen Tuniev <arsen.tuniev.study@gmail.com>
2024-06-02 23:32:24 +0200

    Solve task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (pos) {
+    pos = parseInt(pos);
+    let result = 1;
+    if (pos < 0) return "OOPS";
+    if (pos === 0) return 0;
+    if (pos === 1) return 1;
+    result = fibonacci(pos - 1) + fibonacci(pos - 2);
+    return result;
 };
 
 // Do not edit below this line
