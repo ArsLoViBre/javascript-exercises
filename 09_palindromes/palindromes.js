e3e198f094c8a6bbcf5f65ea59433550b8c1d4c3
Arsen Tuniev <arsen.tuniev.study@gmail.com>
2024-06-02 21:50:05 +0200

    Solve task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    clearedStr = str.replace(/\W/g, '').toLowerCase();
+    divider = Math.floor(clearedStr.length / 2);
+    firstHalf = clearedStr.slice(0, divider);
+    if (clearedStr.length % 2 !== 0) divider += 1;
+    secondHalf = clearedStr.slice(divider, clearedStr.length);
+    secondHalfReversed = secondHalf.split("").reverse().join("");
+    if (firstHalf === secondHalfReversed) return true;
+    return false;
 };
 
 // Do not edit below this line
