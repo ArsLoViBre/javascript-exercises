b50c9f8eb30feb1abf103c35d46e239a6455ef9f
Arsen Tuniev <arsen.tuniev.study@gmail.com>
2024-06-02 19:05:20 +0200

    Sove task 04 and minor correction to 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,13 +3,10 @@ const reverseString = function (str) {
     for (i = 1; i <= str.length; i++) {
         reversedStr += str.charAt(str.length - i);
     }
-    // console.log(str.charAt(-1))
-    // console.log(reversedStr);
 
     return reversedStr;
 };
 
-// reverseString("Hello");
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...values) {
+    values.forEach((val) =>
+        arr = arr.filter((arrVal) => arrVal !== val)
+    );
+    return arr;
 };
+
+console.log(removeFromArray([1, 2, 3], "1", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
