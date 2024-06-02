64ee2c2dd7a98fe85dbc02f917eb22305e554a21
Arsen Tuniev <arsen.tuniev.study@gmail.com>
2024-06-02 19:28:54 +0200

    Solved task 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (typeof (num1) !== 'number' || typeof (num2) !== 'number') return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (num2 < num1) num2 = [num1, num1 = num2][0];
+    sumNum = 0;
+    for (i = num1; i <= num2; i++) {
+        sumNum += i;
+    }
+    return sumNum;
 };
 
 // Do not edit below this line
