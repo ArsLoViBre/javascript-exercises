c9a0d8741fa44f350ce6fc93139493f18d959cc2
Arsen Tuniev <arsen.tuniev.study@gmail.com>
2024-06-03 10:44:53 +0200

    Solve task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    let oldestPerson = {name: '', age: 0};
+    for (i = 0; i < arr.length; i++) {
+        let yearOfBirth = arr[i].yearOfBirth;
+        let yearOfDeath = arr[i].yearOfDeath;
+        yearOfDeath = typeof yearOfDeath !== 'undefined' ? yearOfDeath : new Date().getFullYear();
+        let age = yearOfDeath - yearOfBirth;
+        if (oldestPerson.age < age) {
+            oldestPerson.name = arr[i].name;
+            oldestPerson.age = age;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
